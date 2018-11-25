8d023e812dbab847803893eddc45c13bf07c3aeb
seongenie <lsj234027@gmail.com>
2018-11-25 23:45:27 +0900

    upload dev environment

---
+++ src/components/foundation/button/Button.vue
@@ -0,0 +1,24 @@
+<template>
+  <button
+    :class="['mdl-button', 'mdl-js-button',
+      'mdl-button--raised', 'mdl-js-ripple-effect',
+      'mdl-button--accent', buttonStyle]"
+    @click="click"
+  >
+  {{title}}
+  </button>
+</template>
+<script>
+export default {
+  name: 'Button',
+  props: {
+    title: { type: String },
+    buttonStyle: { type: String }
+  },
+  methods: {
+    click() {
+      this.$emit('onClick');
+    }
+  }
+};
+</script>
